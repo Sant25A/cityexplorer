473b962139a24a8594d3288ed2f90c9c4d67b455
Sant25A <anniitzel.25@gmail.com>
2026-03-12 00:16:04 -0600

    Configura tema CityExplorer + paleta + fuente Outfit

--- frontend/angular-app/tailwind.config.js
+++ frontend/angular-app/tailwind.config.js
@@ -5,7 +5,42 @@ module.exports = {
     "./projects/**/*.{html,ts}", // Añadimos esto por si acaso
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Outfit", "sans-serif"],
+      },
+    },
   },
   plugins: [require("daisyui")],
+
+  daisyui: {
+    themes: [
+      {
+        cityexplorerlight: {
+          "primary": "#065B9C",
+          "secondary": "#31B9F5",
+          "accent": "#5FCAF8",
+          "neutral": "#2B3440",
+          "base-100": "#EDDDD4",
+          "info": "#A1DFFA",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+      {
+        cityexplorerdark: {
+          "primary": "#31B9F5",
+          "secondary": "#5FCAF8",
+          "accent": "#A1DFFA",
+          "neutral": "#1f2937",
+          "base-100": "#111827",
+          "info": "#A1DFFA",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
 }
