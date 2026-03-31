8e6dee3fef9ebbf13f71b5e284575ee05446ccab
Sant25A <anniitzel.25@gmail.com>
2026-03-30 22:17:03 -0600

    Logro: Implementacion de pagina de listado de lugares con filtros y cards reusables

--- frontend/angular-app/src/app/features/places/pages/places-list/places-list.ts
+++ frontend/angular-app/src/app/features/places/pages/places-list/places-list.ts
@@ -1,9 +1,53 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { PlaceCard } from '../../../../shared/components/place-card/place-card';
 
 @Component({
   selector: 'app-places-list',
-  imports: [],
+  imports: [CommonModule, FormsModule, PlaceCard],
   templateUrl: './places-list.html',
   styleUrl: './places-list.css',
 })
-export class PlacesList {}
+export class PlacesList {
+
+  search = '';
+  selectedCity = '';
+  selectedCategory = '';
+
+  places = [
+    {
+      id: 1,
+      name: "Café Central",
+      location: "CDMX",
+      category: "Café",
+      rating: 4.5,
+      image: "https://picsum.photos/400/300?1"
+    },
+    {
+      id: 2,
+      name: "Parque México",
+      location: "CDMX",
+      category: "Parque",
+      rating: 4.7,
+      image: "https://picsum.photos/400/300?2"
+    },
+    {
+      id: 3,
+      name: "Museo Frida Kahlo",
+      location: "CDMX",
+      category: "Museo",
+      rating: 4.8,
+      image: "https://picsum.photos/400/300?3"
+    },
+    {
+      id: 4,
+      name: "Restaurante Azul",
+      location: "CDMX",
+      category: "Restaurante",
+      rating: 4.6,
+      image: "https://picsum.photos/400/300?4"
+    }
+  ];
+
+}
