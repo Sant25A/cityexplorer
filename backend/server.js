f325a08d7cb0cd7e1f6550507ca65f7d93952532
Sant25A <anniitzel.25@gmail.com>
2026-05-24 01:31:12 -0600

    Backend: Configuracion de ruta de produccion para el frontend

--- backend/server.js
+++ backend/server.js
@@ -16,12 +16,7 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-// CORS local
-app.use(cors({
-  origin: 'http://localhost:4200',
-  credentials: true
-}));
-// CORS producción
+// CORS
 const allowedOrigins = [
   'http://localhost:4200',
   process.env.FRONTEND_URL
